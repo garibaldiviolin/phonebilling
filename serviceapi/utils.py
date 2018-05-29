89b7536c2d125de3faa130b6ef67f884ec15d607
garibaldiviolin <garibaldiviolin@gmail.com>
2018-05-29 12:58:21 -0300

    More docstrings

--- serviceapi/utils.py
+++ serviceapi/utils.py
@@ -12,13 +12,16 @@ BILL_PERIOD_MASK = '99/9999'
 STANDING_CALL_CHARGE = 0.36
 STANDARD_MINUTE_CALL_CHARGE = 0.09
 
-NO_COST_PERIOD_START_HOUR = 22
-NO_COST_PERIOD_START_MINUTE = 0
-NO_COST_PERIOD_START_SECOND = 0
+FREE_PERIOD_START_HOUR = 22
+FREE_PERIOD_START_MINUTE = 0
+FREE_PERIOD_START_SECOND = 0
 
-NO_COST_PERIOD_END_HOUR = 6
-NO_COST_PERIOD_END_MINUTE = 0
-NO_COST_PERIOD_END_SECOND = 0
+FREE_PERIOD_END_HOUR = 6
+FREE_PERIOD_END_MINUTE = 0
+FREE_PERIOD_END_SECOND = 0
+
+BASE_PERIOD = 60  # currently, the base period is 1 minute (60 seconds)
+PERIOD_COST = 0.09  # currently, the base period cost is R$ 0,09
 
 
 def subtract_one_month(date):
@@ -35,6 +38,12 @@ def add_one_month(date):
 
 
 def calculate_call_cost(start_time, end_time):
+    ''' This function calculates the phone call's cost, based on the
+    start_time end_time parameters (both are datetime.datetime types)
+    The idea is to use a auxiliar variable (aux_time) to go from start_time
+    to end_time, and sum each period's cost (using the current value of
+    BASE_PERIOD and PERIOD_COST)
+    '''
 
     aux_time = start_time
 
@@ -46,38 +55,41 @@ def calculate_call_cost(start_time, end_time):
         delta_left_time = end_time - aux_time
         delta_seconds = delta_left_time.seconds
 
-        no_cost_period_start = aux_time.replace(
-            hour=NO_COST_PERIOD_START_HOUR,
-            minute=NO_COST_PERIOD_START_MINUTE,
-            second=NO_COST_PERIOD_START_SECOND
+        free_period_start = aux_time.replace(
+            hour=FREE_PERIOD_START_HOUR,
+            minute=FREE_PERIOD_START_MINUTE,
+            second=FREE_PERIOD_START_SECOND
         )
-        no_cost_period_end = aux_time.replace(
-            hour=NO_COST_PERIOD_END_HOUR,
-            minute=NO_COST_PERIOD_END_MINUTE,
-            second=NO_COST_PERIOD_END_SECOND
+        free_period_end = aux_time.replace(
+            hour=FREE_PERIOD_END_HOUR,
+            minute=FREE_PERIOD_END_MINUTE,
+            second=FREE_PERIOD_END_SECOND
         )
 
-        if aux_time > no_cost_period_end:
-            no_cost_period_end = no_cost_period_end \
+        if aux_time > free_period_end:
+            free_period_end = free_period_end \
                 + datetime.timedelta(days=1)
 
-        if (delta_seconds / 60) < 1:
+        # if the left period has less than BASE_PERIOD, than the function can
+        # return because these period isn't charged
+        if (delta_seconds / BASE_PERIOD) < 1:
             return call_cost
 
-        condition_a = aux_time.hour >= 6
-        condition_b = aux_time.hour < 22
+        condition_a = aux_time.hour >= FREE_PERIOD_END_HOUR
+        condition_b = aux_time.hour < FREE_PERIOD_START_HOUR
 
-        # charge for the time
+        # if aux_time is in the period that charges per minute, then
+        # calculate the cost using BASE_PERIOD and PERIOD_COST
         if condition_a and condition_b:
-            delta_charge_time = no_cost_period_start - aux_time
+            delta_charge_time = free_period_start - aux_time
             if (delta_charge_time >= delta_left_time):
                 aux_seconds = delta_seconds
             else:
                 aux_seconds = delta_charge_time.seconds
-            call_cost += (aux_seconds / 60) * 0.09
+            call_cost += (aux_seconds / BASE_PERIOD) * PERIOD_COST
         # don't charge
         else:
-            delta_free_time = no_cost_period_end - aux_time
+            delta_free_time = free_period_end - aux_time
             if (delta_free_time >= delta_left_time):
                 aux_seconds = delta_seconds
             else:
