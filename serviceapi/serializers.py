d00f047bf8d339d63e1a5c69918339273e69261f
garibaldiviolin <garibaldiviolin@gmail.com>
2018-06-11 21:46:55 -0300

    more validations to serializers.py

--- serviceapi/serializers.py
+++ serviceapi/serializers.py
@@ -51,13 +51,29 @@ class CallRecordSerializer(serializers.Serializer):
         # if the request is a partial update, then the record must exist
         if self.partial is True:
             if data['type'] == RecordType.START.value:
-                pdb.set_trace()
                 queryset_start_record = StartRecord.objects. \
                     filter(id=data['id'])
                 if len(queryset_start_record) < 1:
                     raise serializers.ValidationError({
                         'start_record': 'Start record does not exist'
                     })
+
+                record = dict()
+
+                record['id'] = data['id']
+                record['timestamp'] = data.get(
+                    'timestamp', queryset_start_record[0].timestamp
+                )
+                record['call_id'] = data.get(
+                    'call_id', queryset_start_record[0].call_id
+                )
+                record['type'] = data['type']
+                record['source'] = data.get(
+                    'source', queryset_start_record[0].source
+                )
+                record['destination'] = data.get(
+                    'destination', queryset_start_record[0].destination
+                )
             else:  # end record
                 queryset_end_record = EndRecord.objects. \
                     filter(id=data['id'])
@@ -66,53 +82,109 @@ class CallRecordSerializer(serializers.Serializer):
                         'end_record': 'EndStart record does not exist'
                     })
 
+                record = dict()
+
+                record['id'] = data['id']
+                record['timestamp'] = data.get(
+                    'timestamp', queryset_end_record[0].timestamp
+                )
+                record['call_id'] = data.get(
+                    'call_id', queryset_end_record[0].start_id
+                )
+                record['type'] = data['type']
+
+        else:  # create (POST) or complete update (PUT)
+            record = dict()
+
+            record['id'] = data['id']
+            record['timestamp'] = data['timestamp']
+            record['call_id'] = data['call_id']
+            record['type'] = data['type']
+
+            if record['type'] == RecordType.START.value:
+                if 'source' not in data:
+                    raise serializers.ValidationError({
+                        'source': 'Start record must have a source field'
+                    })
+                if 'destination' not in data:
+                    raise serializers.ValidationError({
+                        'destination':
+                        'Start record must have a destination field'
+                    })
+
+                record['source'] = data['source']
+                record['destination'] = data['destination']
+
+        # check if exist a record with the same call_id and type but different
+        # id
+        if record['type'] == RecordType.START.value:
+            queryset_start_record = StartRecord.objects \
+                .exclude(id=record['id']) \
+                .filter(call_id=record['call_id'])
+            if len(queryset_start_record) > 0:
+                raise serializers.ValidationError({
+                    'call_id':
+                    'There is already a start record with this call_id'
+                })
+        else:  # end record
+            queryset_end_record = EndRecord.objects \
+                .exclude(id=record['id']) \
+                .filter(start_id=record['call_id'])
+            if len(queryset_end_record) > 0:
+                raise serializers.ValidationError({
+                    'call_id':
+                    'There is already a end record with this call_id'
+                })
+
         # StartRecord fields validation
-        if data['type'] == RecordType.START.value:
+        if record['type'] == RecordType.START.value:
 
             # source and destination fields must exist for StartRecord
             # Both of then should have 10 or 11 digits (the first two
             # digits are the area code, and the other ones are the phone
             # number), and they must have different values
-            if self.partial is False and 'source' not in data:
-                raise serializers.ValidationError({
-                    'source': 'Start record must have a source field'
-                })
-            elif 'source' in data and data['source'].isdigit() is False:
+            if record['source'].isdigit() is False:
                 raise serializers.ValidationError({
                     'source': 'Source must have only numbers'
                 })
-            elif 'source' in data and len(data['source']) != 10 \
-                    and len(data['source']) != 11:
+            elif 'source' in record and len(record['source']) != 10 \
+                    and len(record['source']) != 11:
                 raise serializers.ValidationError({
                     'source': 'Source must have 10 or 11 digits'
                 })
 
-            if self.partial is False and 'destination' not in data:
-                raise serializers.ValidationError({
-                    'destination': 'Start record must have a destination field'
-                })
-            elif 'destination' in data and data['destination'].isdigit() \
-                    is False:
+            if record['destination'].isdigit() is False:
                 raise serializers.ValidationError({
                     'destination': 'Destination must have only numbers'
                 })
-            elif 'destination' in data and len(data['destination']) != 10 and \
-                    len(data['destination']) != 11:
+            elif len(record['destination']) != 10 \
+                    and len(record['destination']) != 11:
                 raise serializers.ValidationError({
                     'destination': 'Destination must have 10 or 11 digits'
                 })
 
-            if 'source' in data and 'destination' in data and \
-                    data['source'] == data['destination']:
+            if record['source'] == record['destination']:
                 raise serializers.ValidationError({
                     'source':
                     'Source and destination must have different values'
                 })
+
+            queryset_end_record = EndRecord.objects.filter(
+                start_id=record['call_id']
+            )
+            start_timestamp = record['timestamp'].replace(tzinfo=timezone.utc)
+            if len(queryset_end_record) > 0 and \
+                    start_timestamp >= queryset_end_record[0].timestamp:
+                raise serializers.ValidationError({
+                    'timestamp': 'The call start timestamp must be'
+                    ' earlier than call end timestamp'
+                })
+
         else:  # EndRecord fields validation
 
             # StartRecord with the call_id informed must exist
             # before saving EndRecord
-            queryset = StartRecord.objects.filter(call_id=data['call_id'])
+            queryset = StartRecord.objects.filter(call_id=record['call_id'])
 
             if len(queryset) < 1:
                 raise serializers.ValidationError({
@@ -122,7 +194,7 @@ class CallRecordSerializer(serializers.Serializer):
             start_record = queryset[0]
 
             # replace the default timezone (it is not needed)
-            end_timestamp = data['timestamp'].replace(tzinfo=timezone.utc)
+            end_timestamp = record['timestamp'].replace(tzinfo=timezone.utc)
 
             if start_record.timestamp >= end_timestamp:
                 raise serializers.ValidationError({
@@ -133,8 +205,6 @@ class CallRecordSerializer(serializers.Serializer):
         return data
 
     def create(self, validated_data):
-
-        pdb.set_trace()
 
         # if the type is START, then save a start record
         # otherwise, it is a end record
@@ -182,16 +252,16 @@ class CallRecordSerializer(serializers.Serializer):
                 get('timestamp', end_record.timestamp). \
                 replace(tzinfo=timezone.utc)
             end_record.start_id = validated_data.get(
-                'start_id', end_record.start_id
+                'call_id', end_record.start_id
             )
 
-            
-
-            start_record = StartRecord.objects.get(call_id=end_record.start_id)
-            end_record.start = start_record
+            start_record = StartRecord.objects.filter(
+                call_id=end_record.start_id
+            )
+            end_record.start = start_record[0]
 
             end_record.cost = calculate_call_cost(
-                start_record.timestamp, end_record.timestamp
+                start_record[0].timestamp, end_record.timestamp
             )
 
             end_record.save()
