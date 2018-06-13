a2ddaea3e25a827628a82f77349755cc1dcb4d86
garibaldiviolin <garibaldiviolin@gmail.com>
2018-06-13 20:41:45 -0300

    unnecessary code and comments removed from models.py

--- serviceapi/models.py
+++ serviceapi/models.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import pdb
-
-from django.dispatch import receiver
-from django.db.models.signals import pre_save
 from django.db import models
 from serviceapi.utils import RecordType
 
@@ -41,24 +37,3 @@ class EndRecord(models.Model):
     @property
     def call_id(self):
         return self.start_id
-
-    '''@receiver(pre_save)
-    def pre_save_end_record(sender, instance, *args, **kwargs):
-
-        pdb.set_trace()
-
-        queryset_endrecord = EndRecord.objects.filter(id=instance.id)
-        if len(queryset_endrecord) > 1:
-            old_end_record = queryset_endrecord[0]
-            start_record = StartRecord.objects.get(
-                call_id=old_end_record.start_id
-            )
-
-            EndRecord.objects.filter(id=old_end_record.id).delete()
-            StartRecord.objects.filter(id=start_record.id).delete()
-
-            start_record.call_id = instance.start_id
-            start_record.save()
-
-        # User object updated
-        return instance'''
